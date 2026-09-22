e4fb2fb6ed39176ceb10dffbc9707036bf6a7119
Soumyo001 <www.soumyo@gmail.com>
2026-09-22 11:18:17 +0600

    ADD: in-memort json data loader

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"os"
 	_ "rental-property-api/routers"
+	"rental-property-api/services"
 
 	logs "github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
@@ -14,7 +16,12 @@ func main() {
 	}
 
 	datapath := beego.AppConfig.DefaultString("datapath", "data/rental_properties.json")
-	logs.Info("Loaded data file from: %v", datapath)
+	if err := services.Init(datapath); err != nil {
+		logs.Critical("could not load data from %s: %v", datapath, err)
+		os.Exit(1)
+	}
+
+	logs.Info("Loaded %d properties from file %s", services.GetStore().Count(), datapath)
 
 	logs.Info("Server running at http://localhost:8080...")
 	beego.Run()
--- services/property.go
+++ services/property.go
@@ -1,1 +1,60 @@
 package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"rental-property-api/models"
+)
+
+type Store struct {
+	properties []models.SourceProperty
+	index      map[string]int
+}
+
+var defaultStore *Store
+
+func (s *Store) Count() int {
+	return len(s.properties)
+}
+
+func newStore(records []models.SourceProperty) *Store {
+	index := make(map[string]int, len(records))
+	for i, record := range records {
+		index[record.ID] = i
+	}
+
+	return &Store{
+		properties: records,
+		index:      index,
+	}
+}
+
+func loadFromFile(path string) (*Store, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", path, err)
+	}
+
+	var decoded []models.SourceProperty
+	if err := json.Unmarshal(data, &decoded); err != nil {
+		return nil, fmt.Errorf("failed to decode file %s: %w", path, err)
+	}
+
+	return newStore(decoded), nil
+}
+
+func Init(path string) error {
+	store, err := loadFromFile(path)
+	if err != nil {
+		return err
+	}
+
+	defaultStore = store
+	return nil
+}
+
+func GetStore() *Store {
+	return defaultStore
+}
